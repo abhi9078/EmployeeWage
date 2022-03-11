af18570547926a4bafa00b2f06bb298147f8ce12
Abhilash <abhilashmeher1234@gmail.com>
2022-03-11 10:21:55 +0530

    Added UC-2 Files

--- Program.cs
+++ Program.cs
@@ -20,10 +20,34 @@ namespace EmployeeWages
 
         }
 
+        public static void UC2()
+        {
+            Console.WriteLine("Wlcome to UC_2");
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Empoloyee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Total Daily wage is: " + dailyWage);
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Employeewages problem ");
             Program.UC1();
+            Program.UC2();
         }
     }
 }
