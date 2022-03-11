78ad2ff962f6dde3a025e5d56b658a42769d6b44
Abhilash <abhilashmeher1234@gmail.com>
2022-03-11 10:17:27 +0530

    UC-1 File added

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    internal class Program
+    {
+        public static void UC1()
+        {
+            Console.WriteLine("Wlcome to UC_1");
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Employeewages problem ");
+            Program.UC1();
+        }
+    }
+}
